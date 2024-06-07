5d4994446187c89d16dcddfabe62aa17ef4ee195
pkervegan <paul.kervegan@inha.fr>
2024-06-07 16:31:18 +0200

    orm update

--- backend/app/orm/data_sources.py
+++ backend/app/orm/data_sources.py
@@ -131,7 +131,7 @@ class Iconography(db.Model):
         return { "id_uuid"          : self.id_uuid,                      # str
                  "iiif_url"         : self.iiif_url,                     # str
                  "iiif_folio"       : self.iiif_folio,                   # t.List[int] | None
-                 "source_url"       : self.source_url,                   # str
+                 "source_url"       : self.source_url ,                   # str
                  "date"             : int4range2list(self.date),         # t.List[int]
                  "date_source"      : self.date_source,                  # str
                  "technique"        : self.technique,                    # str
