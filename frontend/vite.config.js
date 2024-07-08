29fd67bdb0eb832843744e6816f9e9cedda39837
pkervegan <paul.kervegan@inha.fr>
2024-07-08 11:55:40 +0200

    config issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
 
   // exit if `--mode` was not provided or
   // if it isn't one of `allowedModes`
-  const allowedModes = [ "backend-server", "backend-local" ]
+  const allowedModes = [ "backend-server", "backend-local" ];
   if ( mode==null || !allowedModes.includes(mode) ) {
     console.log("INVALID PARAMETERS\n",
                 `  please provide '--mode' with one of [${allowedModes.map(e => "'"+e+"'")}] and start again.\n`,
@@ -23,33 +23,31 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
     process.exit(1)
   }
 
-  // console.log("%%%%%%%", command, mode, isSsrBuild, isPreview);
+  console.log("%%%%%%%", command, mode, isSsrBuild, isPreview);
 
   // choose the configuration based on `mode`
   let configVariables;
   switch (mode) {
     case "backend-server":
       configVariables = {
-        __API_URL__: JSON.stringify("http://172.17.1.142:5000"),
+        __API_URL__   : JSON.stringify("http://172.17.1.142:5000"),
         __SERVER_URL__: JSON.stringify("http://richdata01.inha.fr")
       };
       break;
     case "backend-local":
       configVariables = {
-        __API_URL__: JSON.stringify("http://localhost:5000/i"),
+        __API_URL__   : JSON.stringify("http://localhost:5000/i"),
         __SERVER_URL__: JSON.stringify("http://richdata01.inha.fr")
       };
       break;
   };
 
   return {
-    plugins: [
-      vue(),
-    ],
+    plugins: [ vue() ],
     define: configVariables,
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        "@": fileURLToPath(new URL('./src', import.meta.url)),
         "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
         "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
         "@router": fileURLToPath(new URL("./src/router", import.meta.url)),
