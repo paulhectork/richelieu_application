815d3e55825c1a0992f3df30170e00045c726090
pkervegan <paul.kervegan@inha.fr>
2024-09-03 18:14:01 +0200

    add article

--- frontend/src/components/ArticleContentProstitution.vue
+++ frontend/src/components/ArticleContentProstitution.vue
@@ -1,9 +1,214 @@
 <template>
-<h1>Article12</h1>
+  <!--
+    images and footnotes:
+    <UiButtonEllipsis data-key="..."></UiButtonEllipsis>
+    <UiButtonEye data-key="an UUID of iiifIdUuid"></UiButtonEye>
+   -->
+
+  <!-- title and subtitle -->
+  <div class="article-header">
+    <h1> La prostitution au Palais-Royal</h1>
+    <h2>Les logiques spatiales du plus vieux métier du monde</h2>
+  </div>
+
+  <!-- the article itself -->
+  <div class="article-body">
+    <p>Propriété du Duc d'Orléans, le
+      <RouterLink to="/sujet/qr169af23423c6c4d5a944bcd1eaf284f12">
+        Palais-Royal</RouterLink> est fermé
+      à la police royale jusqu'en 1789, favorisant de ce
+      fait l'implantation d'activités illicites telles que
+      la <RouterLink to="/theme/qr1bbbf4ba311694ab0be79f2c73ed466f8">
+        prostitution</RouterLink>&nbsp;
+      <UiButtonEye data-key="qr1f366f03581f64c9097dbde348783375b"></UiButtonEye>.
+      Ce phénomène se voit consolider
+      par le déclin des grands bordels d'Ancien Régime à
+      partir de 1770 et la dépénalisation de la prostitution
+      en 1791 dans le cadre de la refonte des codes criminel
+      et correctionnel par les révolutionnaires. Le rôle
+      fondamental du Palais-Royal dans la scène galante parisienne
+      est en outre magnifié par une iconographie pléthorique,
+      mais aussi par la littérature pornographique de l'époque
+      <UiButtonEllipsis data-key="messina"></UiButtonEllipsis>
+      &mdash; Rétif de la Bretonne, Godard D'Aucour ou encore
+      François-Antoine Chevrier.
+    </p>
+    <p>Ce qui rend la prostitution du Palais-Royal exceptionnelle,
+      c'est l'intime articulation qu'elle opère entre l'espace
+      de promenade, l'espace de lotissement et l'espace marchand
+      qui la bordent. Comme l'a montrée Clyde Plumauzille,
+      <q>&nbsp;ces dispositions sont renforcées par la concentration
+      de ces attributs en un périmètre rectangulaire délimité
+      qui en fait non seulement un espace de circulation,
+      mais également un point de rassemblement, agencé par
+      un ensemble de rituels d'exposition
+      <UiButtonEllipsis data-key="plumauzille"></UiButtonEllipsis>&nbsp;</q>.
+
+    </p>
+    <p>Les stratégies territoriales du racolage se déploient dans
+      trois principaux espaces. Le
+      <RouterLink to="/theme/qr119ea3994f787479095683b7da89ccd69">
+        jardin</RouterLink>, d'abord, lieu
+      privilégié de la promenade, contribue au déploiement
+      d'un <q>&nbsp;dispositif tout entier voué au plaisir
+      du voir et de l'être-vu dans la déambulation et la
+      rencontre
+      <UiButtonEllipsis data-key="dautresme"></UiButtonEllipsis>&nbsp;</q>.
+      Puis, les galeries qui, grâce
+      à l'éclairage artificiel de leur
+      <RouterLink to="/theme/qr1b56ce7199bc94c7db5cfbcbb2849220d">
+        mobilier urbain</RouterLink>, permettent
+      de déjouer les pièges de la nuit en assurant une animation
+      dense et constante. Enfin les espaces récréatifs tels
+      que les
+      <RouterLink to="/theme/qr1ccc4169a977f41a995771d81522941e2">
+        théâtres</RouterLink>
+      où il est aisé de trouver des clients
+      friands de divertissement en tous genres. Le
+      <RouterLink to="/sujet/qr1aad3b9f3ee034ba7ba704504f94a38d1">
+        théâtre Montensier</RouterLink>&nbsp;
+      <UiButtonEye data-key="qr18b29fb05163e40079cd62337e6c30a3e"></UiButtonEye>
+      et le
+      <RouterLink to="/sujet/qr14d2e16631b5f474593805d4584bf7466">
+        théâtre Séraphin</RouterLink>&nbsp;
+      <UiButtonEllipsis data-key="deterville"></UiButtonEllipsis>
+      constituent deux des terrains de chasse favoris des Vénus du palais.
+    </p>
+    <p>Les lieux de la besogne sont eux aussi clairement
+      identifiables. Les bancs installés le long des allées
+      du jardin
+      <UiButtonEye data-key="qr11574545f329f4c8dba68abf0dd7fc9ce"></UiButtonEye>
+       offrent la possibilité d'effectuer
+      des actes sexuels rapides et discrets de jour comme
+      de nuit. Les différents péristyles, ménageant des espaces
+      discrets, servent également d'enclave du plaisir. Si
+      les passes se déroulent souvent au grand jour, elles
+      se déroulent également dans l'espace privatif des lotissements
+      du Palais-Royal. Les prostituées font alors cascader
+      leur vertu depuis les fenêtres des maisons de prostitution
+      en séduisant visuellement le badaud, mais aussi auditivement
+      par le sifflement <q>&nbsp;Chit Chit&nbsp;! &nbsp;</q>
+      caractéristique des prostituées de l'époque
+      <UiButtonEye data-key="qr139c6cc29772a4f6e9c50792c62e3d306"></UiButtonEye>.
+      Elles subvertissent de ce fait les frontières entre
+      <RouterLink to="/theme/qr1bc9332b2db5f45ee9dec8433307f8e7f">
+        l'espace public</RouterLink> de la promenade et l'espace privé des
+      <RouterLink to="/theme/qr1566b79275c0d49629b4f5010e4b17165">
+        intérieurs</RouterLink>
+      du Palais, tout en échappant à l'action
+      répressive de la police s'exerçant exclusivement sur
+      les lieux publics
+      <UiButtonEllipsis data-key="plumauzille"></UiButtonEllipsis>.
+      Enfin, il existe également un espace
+      boutiquier de la prostitution qui subvertit les codes
+      traditionnels du
+      <RouterLink to="/theme/qr186bb021ce8dd406a873d35dc90fa7089">
+        commerce</RouterLink> respectable du Palais-Royal.
+      Cette zone grise est à l'œuvre dans les galeries dites
+      <q>&nbsp;de pierre&nbsp;</q>, comprenant les galeries
+      <RouterLink to="/sujet/qr1d9ea0fb3a45945aa8d728d1a7361de6d">
+        Montpensier</RouterLink>,
+      <RouterLink to="/sujet/qr1bc4d6d194a6d47e48c2aa01450cd4877">
+        Beaujolais</RouterLink> et
+      <RouterLink to="/sujet/qr1cc53415ebfdc405697c1486d47946b5e">Valois</RouterLink>
+      et celle dite <q>&nbsp;de bois&nbsp;</q>, ancêtre de la
+      <RouterLink to="/sujet/qr17e0ddbb21d394105b4618fc2460e8654">
+        galerie d'Orléans</RouterLink>.
+      Plusieurs estampes de Claude-Louis Desrais
+      <UiButtonEye data-key="qr197a9bff47a4b42ee853f87a17bd51d64"></UiButtonEye>&nbsp;
+      <UiButtonEye data-key="qr16d5a8f1ed8de46268375a80d7a140172"></UiButtonEye>
+      rendent compte de cette pratique exhibitionniste
+      <UiButtonEllipsis data-key="authier"></UiButtonEllipsis>.
+      Loin de l'exubérance de ces <i>peep-shows</i> primitifs,
+      les marchandes d'amour du Palais-Royal officient également
+      chez les modistes chez qui elles trouvent aisément
+      un emploi prétexte. Cette détonante cohabitation ne
+      va pas sans heurt et s'accompagne de nombreuses plaintes
+      construisant une dualité entre le <q>&nbsp;commerce
+      scandaleux&nbsp;</q> des prostituées et le <q>&nbsp;commerce
+      respectable&nbsp;</q> des honnêtes citoyens
+      <UiButtonEllipsis data-key="plumauzille"></UiButtonEllipsis>.
+    </p>
+    <p>Le commerce prostitutionnel du Palais-Royal est donc fonction
+      de son espace, de son
+      <RouterLink to="/theme/qr18fd23c3d4abf44349bbddc372dbf38c0">
+        architecture</RouterLink>,
+      de son agencement et de la variété de ses ressources commerciales et
+      récréatives (boutiques, cafés, théâtres, restaurants,
+      etc.). Cette emprise géographique ne sera véritablement
+      remise en question que sous le règne de Louis-Philippe
+      qui fait interdire prostituées puis maisons de jeux
+      dans l'enceinte du Palais entre 1830 et 1832, contribuant
+      ainsi au redéploiement de cette activité sur les artères
+      des Grands boulevards.
+    </p>
+  </div>
+
+  <!-- author name -->
+  <div class="article-footer">
+    <p>Louise Thiroux</p>
+  </div>
+
 </template>
 
 
 <script setup>
+import { onMounted, ref } from "vue";
+
+
+import UiButtonEye from "@components/UiButtonEye.vue";
+import UiButtonArrow from "@components/UiButtonArrow.vue";
+import UiButtonEllipsis from "@components/UiButtonEllipsis.vue";
+import { urlToFrontendTheme, urlToFrontendNamedEntity } from "@utils/url.js";
+
+/**********************************************/
+
+const emit    = defineEmits(["queryParams", "iiifIdUuidArray", "footnotes"]);  // what to emit to `ArticleMainView.vue`
+
+const iiifIdUuidArray = [ "qr16d5a8f1ed8de46268375a80d7a140172"	// "PRBSVP122-9"
+                        , "qr18b29fb05163e40079cd62337e6c30a3e"	// "PRBNF75"
+                        , "qr1f366f03581f64c9097dbde348783375b"	// "PRBM12"
+                        , "qr197a9bff47a4b42ee853f87a17bd51d64"	// "PRBNF606"
+                        , "qr139c6cc29772a4f6e9c50792c62e3d306"	// "PRBNF608-1 | PRBNF608-2 | PRBNF608-3 | PRBNF608-4"
+                        , "qr11574545f329f4c8dba68abf0dd7fc9ce"	// "PRPM334"
+                        ];  // the UUIDs of iconography ressources from which to display a IIIF image
+const queryParams     = {};  // the parameters to run a backend advanced search query
+const footnotes       = {
+  messina: `Luisa Messina, <q>&nbsp;Les jardins dans la littérature
+            libertine dix-huitième siècle&nbsp;</q>, <i>Le Monde français
+            du dix-huitième siècle</i> [En ligne], vol. 4, no. 1, 2019.
+            DOI&nbsp;: <a href="https://doi.org/10.5206/mfds-ecfw.v4i1.8278">
+              10.5206/mfds-ecfw.v4i1.8278</a>`,
+  plumauzille: `Clyde Plumauzille, <q>&nbsp;Le <q>&nbsp;marché
+                aux putains&nbsp;</q>&nbsp;: économies sexuelles et dynamiques
+                spatiales du Palais-Royal dans le Paris révolutionnaire&nbsp;</q>,
+                <i>Genre, sexualité &amp; société</i> [En ligne], vol. 10,
+                Automne 2013. DOI&nbsp;: <a href="https://doi.org/10.4000/gss.2943">
+                  10.4000/gss.2943</a>`,
+  dautresme: `Olivier Dautresme, <q>&nbsp;La promenade, un loisir
+              urbain universel&nbsp;? L'exemple du Palais-Royal à
+              Paris à la fin du XVIII<sup>e</sup> siècle&nbsp;</q>,
+              <i>Histoire urbaine</i>, 3-1, 2001, p. 83-102.`,
+  authier: `Catherine Authier, <q>&nbsp;Les marchandes d'amour du
+            Palais-Royal&nbsp;</q>, <i>Histoire par l'image</i> [en ligne],
+            consulté le 01/08/2024. URL&nbsp;:
+            <a href="https://histoire-image.org/etudes/marchandes-amour-palais-royal">
+              https://histoire-image.org/etudes/marchandes-amour-palais-royal</a>.`,
+  deterville: `Déterville, <i>Le Palais-Royal ou Les filles en bonne
+               fortune , coup-d'œil rapide  sur le Palais-Royal en
+               général, sur les maisons de jeu, les filles  publiques...
+               Ouvrage plus moral qu'on ne pense</i>, Paris, chez l'Ecrivain,
+               1815, p.108.`,
+};  // { <footnote key>: <footnote html content> }: the footnotes for the current article
+
+/**********************************************/
+
+// when the component is mounted, send data to parent
+onMounted(() => {
+  emit("iiifIdUuidArray", iiifIdUuidArray);
+  emit("queryParams", queryParams);
+  emit("footnotes", footnotes);
+})
 
 </script>
 
