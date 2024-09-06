09562000eb916fd0b078be64b6540f190d094586
pkervegan <paul.kervegan@inha.fr>
2024-09-06 10:37:54 +0200

    minor updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -30,14 +30,14 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
   switch (mode) {
     case "backend-server":
       configVariables = {
-        __API_URL__   : JSON.stringify("http://172.17.1.142:5000"),
-        __SERVER_URL__: JSON.stringify("https://quartier-richelieu-data.inha.fr") // "http://richdata01.inha.fr")
+        __API_URL__    : JSON.stringify("http://172.17.1.142:5000"),
+        __STATICS_URL__: JSON.stringify("https://quartier-richelieu-data.inha.fr") // "http://richdata01.inha.fr")
       };
       break;
     case "backend-local":
       configVariables = {
-        __API_URL__   : JSON.stringify("http://localhost:5000/i"),
-        __SERVER_URL__: JSON.stringify("https://quartier-richelieu-data.inha.fr")//"http://richdata01.inha.fr")
+        __API_URL__    : JSON.stringify("http://localhost:5000/i"),
+        __STATICS_URL__: JSON.stringify("https://quartier-richelieu-data.inha.fr")//"http://richdata01.inha.fr")
       };
       break;
   };
