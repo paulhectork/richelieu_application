6c5a40340266df5f6792574503aa95eab10da08e
pkervegan <paul.kervegan@inha.fr>
2024-10-02 15:31:08 +0200

    update scripts

--- scripts/run_backend.sh
+++ scripts/run_backend.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# launch the backend (needs to have a virtual environment named `env` in the `backend/` dir
+# usage: bash run_backend.sh
+
 dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$dir/../backend/"
 if [ ! -d env ]; 
-then echo "* python virtualenv called 'env' doesn't exist. run 'scripts/install_backend.sh' before. exiting..." && exit 1; 
+then echo "* python virtualenv called 'env' in 'backend/' doesn't exist. run 'python3 -m venv env' before. exiting..." && exit 1;
 fi;
 source env/bin/activate
-python main.py
+pip install -r ./requirements.txt
+python main.py -m dev
 
--- scripts/run_frontend.sh
+++ scripts/run_frontend.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# run the frontend
+# usage: bash run_frontend.sh
+
 dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$dir/frontend"
-npm run dev
+cd "$dir/../frontend"
+npm install
+npm run dev -- --mode backend-local
