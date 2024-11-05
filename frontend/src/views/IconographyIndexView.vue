15c3819f9d6f6be817bd872ca261fd5b3716b000
Frank Bessou <frank.bessou@logilab.fr>
2024-11-05 15:28:07 +0100

    feat: add Download buttons to Iconography page

--- frontend/src/views/IconographyIndexView.vue
+++ frontend/src/views/IconographyIndexView.vue
@@ -14,6 +14,7 @@
     <IndexIconographyFilter :data="dataFull"
                             @iconography-filter="handleIconographyFilter"
     ></IndexIconographyFilter>
+    <DownloadButtonGroup v-if="dataToDownload" :data="dataToDownload" filename="iconography"/>
     <IndexBase display="resource"
                :data="dataFilter"
     ></IndexBase>
@@ -23,7 +24,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import axios from "axios";
 
 import IndexBase from "@components/IndexBase.vue";
@@ -33,6 +34,7 @@ import IndexCount from "@components/IndexCount.vue";
 import IndexIconographyFilter from "@components/IndexIconographyFilter.vue";
 
 import { indexDataFormatterIconography } from "@utils/indexDataFormatter";
+import DownloadButtonGroup from "../components/DownloadButtonGroup.vue";
 
 /******************************************/
 
@@ -40,6 +42,30 @@ const apiTarget = new URL("/i/iconography", __API_URL__);
 const dataFull = ref([]);      // the full index, independent of user filters
 const dataFilter = ref([]);    // the data to pass to `IndexBase.vue`. this can depend on user-defined filters. an array of { href: <url to redirect to when clicking on an item>, img: <url to the background img to display>, text, <text to display> }
 const isLoaded = ref(false);   // switched to true when the data is loaded, will hide the loader and show the index
+
+/**
+ * Mapping between resource uuids and their corresponding flattened data
+ * to pass to `IndexBase.vue` that will be downloaded using the DownloadButtonGroup.
+ *
+ * @type {Record<string, unknown>}
+ */
+const flattenedResources = computed(() =>
+  Object.fromEntries(dataFull.value.map((resource) =>
+    [
+      resource.id_uuid, {
+        title: resource.title.join(', '),
+        iiif_url: resource.iiif_url,
+        authors: resource.authors.map(author => author.entry_name).join(', '),
+        date: resource.date?.join(', ')
+      }
+    ]
+  ))
+);
+
+/**
+ * @type {Array<unknown>}
+ */
+const dataToDownload = computed(() => dataFilter.value.map((resource) => flattenedResources.value[resource.idUuid]));
 
 /******************************************/
 
