cec2687838635b9fc61036fbcad54b880514761f
pkervegan <paul.kervegan@inha.fr>
2024-11-09 02:37:14 +0100

    css bugfixes + overall cleaning

--- frontend/order_by_rel.sql
+++
@@ -1,6 +0,0 @@
-SELECT theme.id, theme.entry_name, COUNT(r_iconography_theme.id) AS c
-FROM theme
-JOIN r_iconography_theme ON theme.id = r_iconography_theme.id_theme
-GROUP BY theme.id
-ORDER BY c DESC;
-
